b21fd57035a289ff31d67f81c2f235a45ffd5310
TilsonM17 <tilsonmat@gmail.com>
2022-11-29 16:44:11 +0100

    fix - add sudo comand

--- vpn.php
+++ vpn.php
@@ -13,7 +13,7 @@ class Vpn
         'list' => 'cyberghostvpn --traffic --country-code',
         'on' => 'sudo cyberghostvpn --traffic --country-code %s --connect',
         'status' => 'cyberghostvpn --status',
-        'stop' => 'cyberghostvpn --stop'
+        'stop' => 'sudo cyberghostvpn --stop'
     ];
    
     public function __construct(int $countImputParameters, array $imputsParameters)
